7de7af75d4b76526b8fdfaf9557a939ca4c51a92
stickfr <20114698+stickfr@users.noreply.github.com>
2024-02-26 17:16:50 -0500

    Added graph to simulator output page, will need to fix CSS

--- client/src/pages/simulator.js
+++ client/src/pages/simulator.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import {useState, useEffect} from 'react';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
 import SimSettings from '../components/simsettings.js';
 import ModelMap from '../components/modelmap.js';
@@ -6,6 +7,28 @@ import './simulator.css';
 
 export default function Simulator() {
   const [ showSim, setShowSim ] = useState(false);          // Show simulator, or show settings?
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    async function fetchJSON() {
+      let r = await fetch('data/infectivity.json')
+      r = await r.json()
+      setData(r)
+    }
+
+    fetchJSON()
+  }, []);
+
+  const rdata = []
+  for (let key in data){
+    let infectAmt = {}
+    infectAmt["name"] = key
+    for(let value in data[key])
+      infectAmt[value] = data[key][value].length
+    //for (let value in key.value)
+    
+    rdata.push(infectAmt)
+  }
 
   return (
     <div className='simulator'>
@@ -20,7 +43,25 @@ export default function Simulator() {
         {showSim && 
           <div className='output'>
             <ModelMap />
-            {/* Put graphs here */}
+            <LineChart
+              width={1500}
+              height={600}
+              data={rdata}
+              margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5,
+              }}
+              >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis type="number" dataKey="name" tickCount={20}/>
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Line type="monotone" dataKey="delta" stroke="#8884d8" dot={false}/>
+              <Line type="monotone" dataKey="omicron" stroke="#82ca9d" dot={false} />
+            </LineChart>
           </div>
         }
       </div>
