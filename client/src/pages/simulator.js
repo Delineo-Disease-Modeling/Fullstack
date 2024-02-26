48e6c9c6513b88b9bd26c8dd0dd7c27331bc4c1e
stickfr <20114698+stickfr@users.noreply.github.com>
2024-02-26 15:26:21 -0500

    Added simulator settings and a better looking map

--- client/src/pages/simulator.js
+++ client/src/pages/simulator.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+import SimSettings from '../components/simsettings.js';
 import ModelMap from '../components/modelmap.js';
 import './simulator.css';
 
 export default function Simulator() {
+  const [ showSim, setShowSim ] = useState(false);          // Show simulator, or show settings?
+
   return (
-    <div>
-      <ModelMap />
+    <div className='simulator'>
+      <div className='container'>
+        {!showSim && 
+          <div className='settings'>
+            <SimSettings />
+            <button onClick={() => setShowSim(true)}>Simulate</button>
+          </div>
+        }
+
+        {showSim && 
+          <div className='output'>
+            <ModelMap />
+            {/* Put graphs here */}
+          </div>
+        }
+      </div>
     </div>
   )
 }
