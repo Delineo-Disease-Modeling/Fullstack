e45428cf069b7f026682351143603df11cfcf28d
Lixing Wu <lixingwu@lixings-air.acasmart.jh.edu>
2024-02-29 17:25:46 -0500

    redid icon, still working on scaling

--- client/src/components/modelmap.js
+++ client/src/components/modelmap.js
@@ -1,73 +1,42 @@
-import React, {useState} from 'react';
-import { MapContainer, Marker, Popup, TileLayer, SVGOverlay } from 'react-leaflet';
-import L, { Icon, Point } from 'leaflet';
-
+import React, { useState } from 'react';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import L from 'leaflet';
 import facility from '../assets/facility.svg';
-
 import 'leaflet/dist/leaflet.css';
 import './modelmap.css';
 
-const position = [36.562036, -96.160775]
+const position = [36.562036, -96.160775];
 
-const iconFacility = new Icon({
-
+// Define custom icon
+const iconFacility = new L.Icon({
   iconUrl: facility,
   iconRetinaUrl: facility,
-  iconAnchor: null,
-  popupAnchor: null,
-  shadowUrl: null,
-  shadowSize: null,
-  shadowAnchor: null,
-  iconSize: new Point(60, 75),
-  className: 'leaflet-div-icon'
+  iconSize: [60, 75],
+  iconAnchor: [30, 75], // Half of icon's width and full height
 });
-
-// var latLngBounds = L.latLngBounds([[32, -130], [13, -100]]);
-// var svgOverlay = L.svgOverlay(facility, latLngBounds, {
-//   opacity: 1.0,
-//   interactive: true
-// });
 
 export default function ModelMap() {
   const [map, setMap] = useState(null);
-  
-  function MapParams(map2) {
-    setMap(map2);
 
-    console.log("hi");
-
-    map.on('zoomend', function() {
-      var currentZoom = map.getZoom();
-
-      console.log(currentZoom);
-  
-  });
-
-  }
-  
   React.useEffect(() => {
-    const L = require("leaflet");
-
+    const L = require('leaflet');
     delete L.Icon.Default.prototype._getIconUrl;
-
     L.Icon.Default.mergeOptions({
-      iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
-      iconUrl: require("leaflet/dist/images/marker-icon.png"),
-      shadowUrl: require("leaflet/dist/images/marker-shadow.png")
+      iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+      iconUrl: require('leaflet/dist/images/marker-icon.png'),
+      shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
     });
-
-    
   }, []);
 
   return (
-    <MapContainer center={position} zoom={13} className='mapcontainer' whenCreated={setMap}>
+    <MapContainer center={position} zoom={13} className="mapcontainer" whenCreated={setMap}>
       <TileLayer
         attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <SVGOverlay bounds={[[51, 0], [51.5, 0.5]]}>
-
-      </SVGOverlay>
+      <Marker position={position} icon={iconFacility} zoomPanOptions={{ minZoom: 10, maxZoom: 18 }}>
+        <Popup>This building is brown.</Popup>
+      </Marker>
     </MapContainer>
   );
 }
