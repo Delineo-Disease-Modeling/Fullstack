1a39d97e6cec231e27fa1875d945496e77b5cab1
stickfr <20114698+stickfr@users.noreply.github.com>
2024-03-10 11:40:21 -0400

    Hitting submit button now sends appropriate paramaters to simulator Also fixed slider looks and stuff like that TODO: read back simulator output and set the graphs accordingly

--- client/src/pages/simulator.js
+++ client/src/pages/simulator.js
@@ -1,4 +1,5 @@
-import {useState} from 'react';
+import { useState } from 'react';
+import axios from 'axios';
 
 import SimSettings from '../components/simsettings.js';
 import ModelMap from '../components/modelmap.js';
@@ -6,21 +7,44 @@ import OutputGraphs from '../components/outputgraphs.js';
 
 import './simulator.css';
 
+var sim_data = null;
+
+function sendSimulatorData({ location, days, pmask, pvaccine, capacity, lockdown, selfiso }) {
+  const data = {
+    location: location,
+    matrices: null,
+    days: days,
+    mask: pmask,
+    vaccine: pvaccine,
+    capacity: capacity,
+    selfiso: selfiso,
+    lockdown: lockdown,
+  };
+
+  axios.post("http://127.0.0.1:5000/simulation/", data)
+    .then((res) => {
+      sim_data = res.data;
+      console.log(res.data);
+    })
+    .catch((error) => {
+      console.log(error.response);
+    });
+}
+
 export default function Simulator() {
   const [ showSim, setShowSim ] = useState(false);          // Show simulator, or show settings?
 
   return (
     <div>
-      <div className='container'>
+      <div className='sim_container'>
         {!showSim && 
-          <div className='settings'>
-            <SimSettings />
-            <button onClick={() => setShowSim(true)}>Simulate</button>
+          <div className='sim_settings'>
+            <SimSettings sendData={sendSimulatorData} showSim={setShowSim}/>
           </div>
         }
 
         {showSim && 
-          <div className='output'>
+          <div className='sim_output'>
             <ModelMap />
             <OutputGraphs />
           </div>
