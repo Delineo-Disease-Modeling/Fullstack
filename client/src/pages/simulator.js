31bafbb1984e984960779f2173b97813b91a5022
stickfr <20114698+stickfr@users.noreply.github.com>
2024-03-11 17:25:40 -0400

    Some other changes

--- client/src/pages/simulator.js
+++ client/src/pages/simulator.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import axios from 'axios';
+//import axios from 'axios';
 
 import SimSettings from '../components/simsettings.js';
 import ModelMap from '../components/modelmap.js';
