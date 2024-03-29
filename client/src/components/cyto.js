841f70a67363965f30f5f62fbd9a4874f9f588ca
Matthew Yu <bionson@Matthews-MacBook-Pro.local>
2024-03-29 17:18:06 -0400

    Fixed people movement in cytoscape graph

--- client/src/components/cyto.js
+++ client/src/components/cyto.js
@@ -3,54 +3,69 @@ import React, { useEffect, useState } from 'react';
 
 class CytoScape extends React.Component {
     render(){
-      const elements = [
-         { data: { id: 'one', label: 'Node 1' }, position: { x: 0, y: 0 } },
-         { data: { id: 'two', label: 'Node 2' }, position: { x: 100, y: 0 } }
-         //{ data: { source: 'one', target: 'two', label: 'Edge from Node1 to Node2' } }
-      ];
-      return <CytoscapeComponent elements={this.props.data} style={ { width: '600px', height: '600px' } } />;
+      return <CytoscapeComponent elements={this.props.data} style={ { width: '1500px', height: '600px' } } />;
     }
   }
 
 
 export default function CytoGraph() {
-    const [data, setData] = useState(null)
+    const [papData, setPapData] = useState(null)
+    const [patData, setPatData] = useState(null)
+    const [tData, settData] = useState(null)
     const elements = []
 
     useEffect(() => {
         async function fetchJSON() {
             let a = await fetch('data/papdata.json')
+            let p = await fetch('data/patterns.json')
+            let t = await fetch('data/pattern_simple.json')
             a = await a.json()
+            p = await p.json()
+            t = await t.json()
             console.log(a)
-            setData(a)
+            console.log(p)
+            console.log("t")
+            console.log(t)
+            setPapData(a)
+            setPatData(p)
+            settData(t)
         }
         fetchJSON()
         }, []);
-    
-    if (data != null){
+    if (papData != null && patData != null){
         let pos = 0;
         let cbg = 401139400082;
         let homes = [];
+        let prevHomes = {}
 
-        for (let key in data["homes"]){
-            if (data["homes"][key]["cbg"] == cbg){
-                elements.push({ data: { id: key, label: key }, position: { x: pos, y: 0 } })
+        for (let key in papData["homes"]){
+                if (parseInt(key)<=5){
+                for (let i = 1; i < Object.keys(patData).length+1; i++){
+                    elements.push({ data: { id: key+"t"+(i*60), label: key }, position: { x: pos, y: i*500 } })
+                }
                 homes.push(key)
+                
+            }
+            pos+=200;
+        }
+        
+        for (let time in patData){
+            let curHomes = {}
+            for (let home in patData[time]["homes"]){
+                if (parseInt(home)<=5){
+                    for(let person in patData[time]['homes'][home]){
+                        if (patData[time]['homes'][home][person] in prevHomes){
+                            elements.push({ data: { source: home+"t"+time, target: prevHomes[patData[time]['homes'][home][person]]+"t"+(parseInt(time)-60), label: 'Edge from Node1 to Node2' } })
+                        }                        
+                        curHomes[patData[time]['homes'][home][person]] = home
+                    }
+                }
             }
             
-            //elements.push({ data: { source: 'test', target: key, label: 'Edge from Node1 to Node2' } })
-            pos+=100;
-        }
-        pos = 0;
-        for (let key in data["people"]){
-            if (homes.includes(data["people"][key]["home"])){
-                elements.push({ data: { id: "p"+key, label: "person" }, position: { x: pos, y: 50 } })
-                elements.push({ data: { source: "p"+key, target: data["people"][key]["home"], label: 'Edge from Node1 to Node2' } })
-            }
-            pos+=100;
+            prevHomes = curHomes
         }
     }
-    console.log(elements)
+    
     return (
     <div>
             <CytoScape data={elements}/>
