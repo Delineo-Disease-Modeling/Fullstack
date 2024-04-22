5e21ee06e7bb157d6714d9af9f7132e96607f294
stickfr <20114698+stickfr@users.noreply.github.com>
2024-04-22 18:45:06 -0400

    Added buttons and input to matrixselector

--- client/src/components/matrixselector.js
+++ client/src/components/matrixselector.js
@@ -7,6 +7,21 @@ const matrix_files = [
   'default1.csv', 'default2.csv'
 ];
 
+// UPDATE THIS ANYTIME YOU WANT TO ADD A NEW DEFAULT VARIANT
+const variant_list = [
+  'delta', 'omicron'
+]
+
+function CustomVariants({ customVariants }) {
+  if (!customVariants) {
+    return null;
+  }
+
+  return Array.from(customVariants).map(variant => {
+    return <option selected value={variant} key={variant}>{variant}</option>
+  });
+}
+
 function CustomMatrices({ customFiles }) {
   if (!customFiles) {
     return null;
@@ -41,6 +56,8 @@ function createFileList(selected, defaultMatrices, customFiles, setMatrices) {
 
 export default function MatrixSelector({ customFiles, setMatrices }) {
   const [defaultMatrices, setDefaultMatrices] = useState({});
+  const [variants, setVariants] = useState([]);
+  const [curVariant, setCurVariant] = useState(null);
 
   useEffect(() => {
     async function loadMatrices() {
@@ -54,20 +71,36 @@ export default function MatrixSelector({ customFiles, setMatrices }) {
       setDefaultMatrices(matrix_dict);
     }
 
+    setVariants(variant_list);
     loadMatrices();
-  }, [setDefaultMatrices]);
+  }, [setDefaultMatrices, setVariants]);
 
   return (
     <div className='mselect_container'>
       <div className='mselect_label'>
         Disease Matrices
       </div>
-      <select 
-        className='mselect_select' size={6} multiple
-        onChange={(e) => createFileList(e.target.selectedOptions, defaultMatrices, customFiles, setMatrices)}>
-        {matrix_files.map(x => 'data/'+x).map(x => <option selected value={x} key={x}>{x}</option>)}
-        <CustomMatrices customFiles={customFiles}/>
-      </select>
+
+      <div>
+        <select
+          className='mselect_selectvariant' size={6}
+          onChange={(e) => setCurVariant(Object.values(e.target.selectedOptions)[0].value)}>
+          <CustomVariants customVariants={variants}/>
+        </select>
+
+        <select 
+          className='mselect_selectmatrix' size={6}
+          onChange={(e) => createFileList(e.target.selectedOptions, defaultMatrices, customFiles, setMatrices)}>
+          {matrix_files.map(x => 'data/'+x).map(x => <option selected value={x} key={x}>{x}</option>)}
+          <CustomMatrices customFiles={customFiles}/>
+        </select>
+      </div>
+
+      <div className='mselect_varadder'>
+        <input></input>
+        <button className='mselect_button'>+</button>
+        <button className='mselect_button'>-</button>
+      </div>
     </div>
   )
 }
