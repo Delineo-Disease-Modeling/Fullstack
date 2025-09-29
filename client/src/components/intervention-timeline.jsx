635be795c348ed2605b1b10171988eba07af4ed2
Stick <20114698+stickms@users.noreply.github.com>
2025-09-29 19:44:23 -0400

    Added buttons to scroll through interventions

--- client/src/components/intervention-timeline.jsx
+++ client/src/components/intervention-timeline.jsx
@@ -85,35 +85,55 @@ export default function InterventionTimeline() {
         ))}
       </div>
 
-      {/* Information */}
-      <div className='flex flex-col gap-4'>
-        <div className='flex w-full items-center justify-center gap-2'>
-          <div className={values.length <= 1 ? 'cursor-not-allowed' : ''}>
-            <button
-              className='timeline bg-red-400 disabled:bg-red-800'
-              onClick={() => deleteThumb(values.indexOf(curtime))}
-              disabled={values.length <= 1}
-            >
-              Delete
-            </button>
-          </div>
-          <div className={values.length >= 10 ? 'cursor-not-allowed' : ''}>
-            <button
-              className='timeline bg-[#222629] disabled:bg-stone-600'
-              onClick={() => {
-                const newvalue = [...Array(settings.hours).keys()]
-                  .filter((v) => !values.includes(v))[0];
-
-                addInterventions(newvalue);
-                setValues((cur) => [...cur, newvalue]);
-                setCurtime(newvalue);
-              }}
-              disabled={values.length >= 10}
-            >
-              + Add Intervention
-            </button>
-          </div>
+      {/* Buttons */}
+      <div className='flex w-full items-center justify-center gap-2'>
+        <div className={curtime === values.sort()[0] ? 'cursor-not-allowed' : ''}>
+          <button
+            className='timeline bg-[#222629] disabled:bg-stone-600 px-4!'
+            onClick={() => setCurtime(() => values.sort()[values.sort().indexOf(curtime) - 1])}
+            disabled={curtime === values.sort()[0]}
+          >
+            &lt;
+          </button>
         </div>
+        <div className={values.length <= 1 ? 'cursor-not-allowed' : ''}>
+          <button
+            className='timeline bg-red-400 disabled:bg-red-800'
+            onClick={() => deleteThumb(values.indexOf(curtime))}
+            disabled={values.length <= 1}
+          >
+            Delete
+          </button>
+        </div>
+        <div className={values.length >= 10 ? 'cursor-not-allowed' : ''}>
+          <button
+            className='timeline bg-[#222629] disabled:bg-stone-600'
+            onClick={() => {
+              const newvalue = [...Array(settings.hours).keys()]
+                .filter((v) => !values.includes(v))[0];
+
+              addInterventions(newvalue);
+              setValues((cur) => [...cur, newvalue]);
+              setCurtime(newvalue);
+            }}
+            disabled={values.length >= 10}
+          >
+            + Add Intervention
+          </button>
+        </div>
+        <div className={curtime === values.sort()[values.length - 1] ? 'cursor-not-allowed' : ''}>
+          <button
+            className='timeline bg-[#222629] disabled:bg-stone-600 px-4!'
+            onClick={() => setCurtime(() => values.sort()[values.sort().indexOf(curtime) + 1])}
+            disabled={curtime === values.sort()[values.length - 1]}
+          >
+            &gt;
+          </button>
+        </div>
+      </div>
+
+      {/* Information & Buttons*/}
+      <div className='flex flex-col gap-4'>
         <h4 className='text-center text-lg'>
           Hour #{curtime.toString().padStart(3, '0')} - {' '}
           {settings.zone && new Date(new Date(settings.zone.start_date).getTime() + curtime * 60 * 60 * 1000).toLocaleString('en-US', {
